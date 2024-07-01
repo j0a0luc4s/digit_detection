4e8af3ed39904b763aa4c1c2be4bc4304b5e1ccb
j0a0luc4s <joaolucasrrolim@gmail.com>
2024-07-01 19:34:30 -0300

    Minor changes

--- src/givens.jl
+++ src/givens.jl
@@ -4,16 +4,16 @@ function givens(
     atol::Tp = 1e-6
 ) where {Tp <: AbstractFloat}
     if isapprox(b, Tp(0.0); atol=atol)
-        s::Tp = 0.0
-        c::Tp = 1.0
+        s = Tp(0.0)
+        c = Tp(1.0)
     elseif abs(b) > abs(a)
-        τ::Tp = -a/b
-        s::Tp = 1.0/sqrt(1.0 + τ*τ)
-        c::Tp = s*τ
+        τ = Tp(-a/b)
+        s = Tp(1.0/sqrt(1.0 + τ*τ))
+        c = Tp(s*τ)
     else
-        τ::Tp = -b/a
-        c::Tp = 1.0/sqrt(1.0 + τ*τ)
-        s::Tp = c*τ
+        τ = Tp(-b/a)
+        c = Tp(1.0/sqrt(1.0 + τ*τ))
+        s = Tp(c*τ)
     end
 
     return Tp[c  s;
--- src/house.jl
+++ src/house.jl
@@ -3,14 +3,14 @@ function house(
     atol::Tp = 1e-6
 ) where {Tp <: AbstractFloat}
     m = length(x)
-    σ::Tp = x[2:m]'*x[2:m]
+    σ = Tp(x[2:m]'*x[2:m])
     v = vcat(1.0, x[2:m])
     
     if isapprox(σ, 0.0; atol=atol)
-        β::Tp = 2.0
+        β = Tp(2.0)
     else
         v[1] = -σ/(x[1] + sqrt(x[1]*x[1] + σ))
-        β::Tp = 2*v[1]*v[1]/(σ + v[1]*v[1])
+        β = Tp(2*v[1]*v[1]/(σ + v[1]*v[1]))
         v = v/v[1]
     end
 
